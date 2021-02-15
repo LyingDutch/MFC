377c75d96d5d34178cb0cd7c93ab20665c16686c
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-02-15 01:20:50 +0100

    Update Remote.h
    fixed convention nameing (Uppercase CONST)

--- Remote.h
+++ Remote.h
@@ -7,25 +7,25 @@ namespace LDKlok
 struct Remote
 {
 public:
-  static const unsigned long rPowerButton = 16748655;
+  static const unsigned long POWERBUTTON = 16748655;
 
-  static const unsigned long rN0 = 16738455;
-  static const unsigned long rN1 = 16724175;
-  static const unsigned long rN2 = 16718055;
-  static const unsigned long rN3 = 16743045;
-  static const unsigned long rN4 = 16716015;
-  static const unsigned long rN5 = 16726215;
-  static const unsigned long rN6 = 16734885;
-  static const unsigned long rN7 = 16728765;
-  static const unsigned long rN8 = 16730805;
-  static const unsigned long rN9 = 16732845;
+  static const unsigned long N0 = 16738455;
+  static const unsigned long N1 = 16724175;
+  static const unsigned long N2 = 16718055;
+  static const unsigned long N3 = 16743045;
+  static const unsigned long N4 = 16716015;
+  static const unsigned long N5 = 16726215;
+  static const unsigned long N6 = 16734885;
+  static const unsigned long N7 = 16728765;
+  static const unsigned long N8 = 16730805;
+  static const unsigned long N9 = 16732845;
 
-  static const unsigned long rNextSegment = 16712445;
-  static const unsigned long rlastSegment = 16720605;
+  static const unsigned long NEXTSEGMENT = 16712445;
+  static const unsigned long LASTSEGMENT = 16720605;
   
-  static const unsigned long rSetTime = 16761405;
-  static const unsigned long rClockPage = 16769565;
-  static const unsigned long rWeatherPage = 16753245;
-  static const unsigned long rHomePage = 16736925;
+  static const unsigned long OPTIONS = 16761405;
+  static const unsigned long CLOCKPAGE = 16769565;
+  static const unsigned long WEATHERPAGE = 16753245;
+  static const unsigned long HOMEPAGE = 16736925;
 };
 }
